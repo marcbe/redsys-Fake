0190f965ccce27b16595ebcf333356f7c8cabffe
Marc <marc@creagia.com>
2017-05-25 19:29:42 +0200

    Show parameters

--- src/views/table-post.php
+++ src/views/table-post.php
@@ -2,15 +2,29 @@
     <tbody>
         <?php foreach ($_POST as $key => $value) { ?>
         <tr>
-            <th><?= $key; ?></th>
-            <td><?= $value; ?></td>
+            <th><?php echo $key; ?></th>
+            <td><?php echo $value; ?></td>
         </tr>
         <?php } ?>
+
+        <?php
+        if (isset($_POST['Ds_MerchantParameters'])) {
+            $parameters = json_decode(base64_decode($_POST['Ds_MerchantParameters']));
+            foreach ($parameters as $key => $value) {
+            ?>
+            <tr>
+                <th><?php echo $key; ?></th>
+                <td><?php echo $value; ?></td>
+            </tr>
+            <?php
+            }
+        }
+        ?>
 
         <?php if (isset($_POST['Ds_Merchant_Amount'])) { ?>
         <tr>
             <th>Total Import (Ds_Merchant_Amount)</th>
-            <td><?= preg_replace('/([0-9]{2})$/', ',$1', $_POST['Ds_Merchant_Amount']); ?>&euro;</td>
+            <td><?php echo preg_replace('/([0-9]{2})$/', ',$1', $_POST['Ds_Merchant_Amount']); ?>&euro;</td>
         </tr>
         <?php } ?>
     </tbody>
